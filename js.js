3463f6405451000377cd3c33b3c78886cba62f38
johnmaverickviana-hue <johnmaverickviana@gmail.com>
2026-04-27 14:46:54 +0800

    first commit

---
+++ js.js
@@ -0,0 +1,5 @@
+const profilePic = document.querySelector('.profile-pic');
+profilePic.style.position = 'absolute';
+profilePic.style.right = '20px';
+profilePic.style.top = '50%';
+profilePic.style.transform = 'translateY(-50%)';
